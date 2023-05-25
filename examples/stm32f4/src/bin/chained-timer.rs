e1fd54c585b77d7986ec603f571e3b736cec9660
Philipp Eitner <peitner@PE-ProBook.fritz.box>
2023-05-25 20:14:54 +0200

    tim1 and tim8 output

--- examples/stm32f4/src/bin/chained-timer.rs
+++ examples/stm32f4/src/bin/chained-timer.rs
@@ -5,6 +5,10 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::pwm::complementary_pwm::{Ckd, ComplementaryPwm, ComplementaryPwmPin};
+use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
+use embassy_stm32::pwm::Channel;
+use embassy_stm32::time::khz;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -14,6 +18,52 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+
+    // TIM1
+    let ch2 = PwmPin::new_ch2(p.PA9);
+    let ch3 = PwmPin::new_ch3(p.PA10);
+    let ch2n = ComplementaryPwmPin::new_ch2(p.PB0);
+    let ch3n = ComplementaryPwmPin::new_ch3(p.PB1);
+    let mut tim1_pwm = ComplementaryPwm::new(
+        p.TIM1,
+        None,
+        None,
+        Some(ch2),
+        Some(ch2n),
+        Some(ch3),
+        Some(ch3n),
+        None,
+        None,
+        khz(20),
+    );
+
+    // TIM8
+    let ch2 = PwmPin::new_ch2(p.PC7);
+    let ch3 = PwmPin::new_ch3(p.PC8);
+    let ch2n = ComplementaryPwmPin::new_ch2(p.PB14);
+    let ch3n = ComplementaryPwmPin::new_ch3(p.PB15);
+    let mut tim8_pwm = ComplementaryPwm::new(
+        p.TIM8,
+        None,
+        None,
+        Some(ch2),
+        Some(ch2n),
+        Some(ch3),
+        Some(ch3n),
+        None,
+        None,
+        khz(20),
+    );
+    // let max = pwm.get_max_duty();
+    tim1_pwm.set_dead_time_clock_division(Ckd::DIV1);
+    tim1_pwm.set_dead_time_value(0);
+    tim8_pwm.set_dead_time_clock_division(Ckd::DIV1);
+    tim8_pwm.set_dead_time_value(0);
+
+    tim1_pwm.enable(Channel::Ch2);
+    tim1_pwm.enable(Channel::Ch3);
+    tim8_pwm.enable(Channel::Ch2);
+    tim8_pwm.enable(Channel::Ch3);
 
     loop {
         info!("high");
